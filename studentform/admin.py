bb669fd21fefd9923a95438204f4d114f03809de
kusum <kusumstha251@gmail.com>
2024-05-31 10:16:39 +0545

    final

--- studentform/admin.py
+++ studentform/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from .models import Student
 
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'first_name', 'last_name', 'age', 'level','email_id', 'phone', 'address', 'grade', 'major',)
-    search_fields = ('first_name', 'student_id', 'major')
-    list_filter = ('major', 'grade')
-    list_per_page= 10
+
+
 
